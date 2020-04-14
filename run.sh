370f95b61e230c6bf8b33226014ace4b5db23ba9
Mirco Zeiss <mirco.zeiss@gmail.com>
2020-04-14 10:20:53 +0200

    add another values to have multiple

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ curl \
 	--verbose \
 	--header "Authorization: Token 6d55cb11-16ee-47f6-b477-45fbe19ca8af" \
 	--header "Content-Type: application/json" \
-	--data "{\"value\":4,\"sha\":\"${CIRCLE_SHA1}\"}" \
+	--data "{\"value\":19,\"sha\":\"${CIRCLE_SHA1}\"}" \
 	"https://dev.mircozeiss.com/api/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/foo/values"
 
 exit 0
